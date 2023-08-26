85a39b60f08f1d574874153ef664048690f34e31
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 02:49:51 +0530

    Create Movie.java

---
+++ main.api/Movie.java
@@ -0,0 +1,16 @@
+public class Movie {
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int durationInMinutes, double imdbRating) {
+        this.name = name;
+        this.durationInMinutes = durationInMinutes;
+        this.imdbRating = imdbRating;
+    }
+
+    // Getters and setters
+}
