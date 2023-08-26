065c8d3427fd723e1582c6291722db70e3f789ff
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 02:50:12 +0530

    Create Director.java

---
+++ main.api/Director.java
@@ -0,0 +1,16 @@
+public class Director {
+    private String name;
+    private int numberOfMovies;
+    private double imdbRating;
+
+    public Director() {
+    }
+
+    public Director(String name, int numberOfMovies, double imdbRating) {
+        this.name = name;
+        this.numberOfMovies = numberOfMovies;
+        this.imdbRating = imdbRating;
+    }
+
+    // Getters and setters
+}
